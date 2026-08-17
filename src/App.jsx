506e58a2f1811c55c7d041f1f17914075db65235
Ritesh Singh <j4260381@gmail.com>
2026-08-17 16:07:34 +0530

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Analytics } from '@vercel/analytics/next';
 import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <>
       <Portfolio />
+      <Analytics/>
     </>
   );
 }
